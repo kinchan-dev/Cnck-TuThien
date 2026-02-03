b5d106a0397782063a7d7debe3d82fe9154bf9bd
Tran Trung Kien <trantrungkieen2809@gmail.com>
2026-02-04 00:26:12 +0700

    chạy trên laptop

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,7 @@
 import dotenv from "dotenv";
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 dotenv.config({ path: new URL("../.env", import.meta.url).pathname });
 
 import { createApp } from "./app.js";
